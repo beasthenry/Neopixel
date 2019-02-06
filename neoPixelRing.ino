ef6b7186639cbf44ca6226049bb2bf0cce00cc92
Beasthenry <lixinhenginfo@gmail.com>
2019-02-06 14:06:15 -0500

    Added arudino files

---
+++ neoPixelRing.ino
@@ -0,0 +1,30 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            12
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      1
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+//#if defined (__AVR_ATtiny85__)
+//  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+//#endif
+  // End of trinket special code
+
+  pixels.begin(); // This initializes the NeoPixel library.
+}
+
+void loop() {
+ pixels.setPixelColor(0, pixels.Color(100,222,0)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+}
